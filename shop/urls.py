82642625cbf8f30b7ab82c5111c86c04071eb776
Ryan Keys <r.keys1998@gmail.com>
2020-03-07 15:01:25 -0800

    added image support

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from shop.views import ItemListView, ItemDetailView, NewItem
+from shop.views import ItemListView, ItemDetailView, NewItem, DeleteItem
 
 urlpatterns = [
     path('', ItemListView.as_view(), name='item-list-page'),
     path('listings/<str:slug>', ItemDetailView.as_view(), name='shop-details-page'),
-    path('new/', NewItem.as_view(),name='item-create')
+    path('new/', NewItem.as_view(),name='item-create'),
+    path('listings/<str:slug>/delete', DeleteItem.as_view(), name='delete-item' )
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.views.generic.list import ListView
-from django.urls import reverse_lazy
-from django.views.generic import DetailView, TemplateView, CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import DetailView, TemplateView, CreateView, DeleteView
 from shop.models import Listing
 from datetime import timezone
 from shop.forms import AddItem
@@ -26,5 +27,16 @@ class ItemDetailView(DetailView):
 
 class NewItem(CreateView):
     form_class = AddItem
-    success_url = reverse_lazy('shop-details-page')
     template_name = 'new_listing.html'
+    success_url = reverse_lazy('item-list-page')
+
+class DeleteItem(DeleteView):
+    model = Listing
+    template_name = 'item_delete.html'
+
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(Listing, slug=slug)
+
+    def get_success_url(self):
+        return reverse('item-list-page')
