c507ee1f0a245b7b187e7bd4a787aa6e1f9bc491
Ryan Keys <r.keys1998@gmail.com>
2020-03-07 17:34:45 -0800

    eeehe

--- contractor_django/settings.py
+++ contractor_django/settings.py
@@ -128,9 +128,9 @@ MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    ]
+)
